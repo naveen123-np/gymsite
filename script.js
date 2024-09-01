ce13a155c69c9b1a8285a12d37d241ba6c4161ca
Naveen pandey <145537811+naveen123-np@users.noreply.github.com>
2024-09-01 10:50:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-import products from 'products.json' with {type: "json"};
+import products from './products.json' with {type: "json"};
 import {showContainer } from './productcard.js';
 showContainer(products);
 console.log(products)
